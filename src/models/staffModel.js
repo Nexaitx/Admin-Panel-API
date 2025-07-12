0c6f38082d1af7e6c8890b6a647c8f9070bb2f96
rukhsana1827 <rukhsana1827asif@gmail.com>
2025-07-12 13:52:52 +0530

    added multiple model for staffs

--- src/models/staffModel.js
+++ src/models/staffModel.js
@@ -4,5 +4,17 @@ const getAllStaffs = async () => {
   const [rows] = await db.query('SELECT * FROM staff');
   return rows;
 };
+const getStaffById = async (id) => {
+  const [rows] = await db.query('SELECT * FROM staff WHERE id = ?', [id]);
+  return rows[0];
+};
 
-module.exports = { getAllStaffs };
+const updateStaff = async (id, staffData) => {
+  await db.query('UPDATE staff SET ? WHERE id = ?', [staffData, id]);
+};
+
+const deleteStaff = async (id) => {
+  await db.query('DELETE FROM staff WHERE id = ?', [id]);
+};
+
+module.exports = { getAllStaffs, getStaffById, updateStaff, deleteStaff };
